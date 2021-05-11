f2183b6e24744d285dbe58bdaa2dc6f4bffad514
justcopy <45755204+justcopy@users.noreply.github.com>
2021-05-11 10:35:19 +0800

    Update jd_super_redrain.js

--- qx/jd_super_redrain.js
+++ qx/jd_super_redrain.js
@@ -240,7 +240,7 @@ async function retryCdn(code, url) {
 }
 
 function rraUrl() {
-    let url = 'https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd-live-rain.json'
+    let url = 'https://raw.githubusercontent.com/justcopy/longzhuzhu/main/qx/jd-live-rain.json'
     if($.isNode() && process.env.JD_RRA_URL){
         url = process.env.JD_RRA_URL
     }else if($.getdata('jdRRAUrl')){
